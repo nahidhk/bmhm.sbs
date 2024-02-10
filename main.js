28eed0935e1fdc01fa182239bb43abdb2e2439fd
NAHID HK <117029405+nahidhk@users.noreply.github.com>
2024-02-10 18:15:29 +0600

    New update
    Add a sampule

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+document.write(`
+
+ <section class="nav"><br>
+   <span class="main-title"><span class="c1">B</span><span class="c3">M</span><span class="c2">H</span><span class="c4">M</span><span class="c5">.</span><span class="c2">SBS</span></span>
+ </section>
+ 
+ <section style="text-align: center">
+   
+  <p>The BMHM.sbs is a Only <a href="https://nahidhk.info">Nahid HK</a> Web Host Server!</p> 
+ </section>
+ <center>
+   <br><br><br><br>
+<button class="abt" onclick="window.location.href='https://linktr.ee/nahidhk'">Open About</button>
+  </center>
+
+ 
+`)
+
+
